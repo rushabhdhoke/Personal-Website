fa26116479e87ba1208532449ded511b07b69e6d
Rushabh Dhoke <78155123+rushabhdhoke@users.noreply.github.com>
2025-05-26 19:32:46 -0400

    Update config.ts
    added links

--- app/config.ts
+++ app/config.ts
@@ -2,14 +2,14 @@ export const metaData = {
   baseUrl: "https://nextfolio-template.vercel.app/",
   title: "Personal Website",
   name: "Rushabh Dhoke",
-  ogImage: "/opengraph-image.png",
+  ogImage: "https://media.licdn.com/dms/image/v2/C4E03AQFlEyp3IPucYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1625729174702?e=1753920000&v=beta&t=LYk15CUWrirQhT9jVYXUS3xghf5EbgrYcdOHv5XjgSg",
   description:
     "Personal Website with all Projects achievements",};
 
 export const socialLinks = {
-  twitter: "https://x.com/?&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  //twitter: "https://x.com/?&",
+  github: "https://github.com/rushabhdhoke",
+  //instagram: "https://www.instagram.com/",
+  linkedin: "https://www.linkedin.com/in/rushabhdhoke23/",
+  email: "mailto:rsd@udel.edu",
 };

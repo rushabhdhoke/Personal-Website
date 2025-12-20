cf78c63411f80e436611a7ebf5ca626de388dc6f
Rushabh Dhoke <78155123+rushabhdhoke@users.noreply.github.com>
2025-12-19 21:28:06 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ const featured = projects.slice(0, 3);
       <div className="flex flex-col items-center text-center">
         <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
           <Image
-            src="https://media.licdn.com/dms/image/v2/C4E03AQFlEyp3IPucYg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1625729174702?e=1767830400&v=beta&t=xnpuNvpk13_8_5BdPs9Bl2gD-KjlyRv45iFEJJzFy3o"
+            src="https://media.licdn.com/dms/image/v2/C4E03AQFlEyp3IPucYg/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1625729174702?e=1767830400&v=beta&t=I9F6n9C-nVp2Q6OiGsDLJ7SoiiWr-xHzsy2FMEPRzSQ"
             alt="Rushabh Dhoke"
             width={160}
             height={160}
